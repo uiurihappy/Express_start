94d6f699df03ff7781c7ea0fad09b710e8028c1e
Yunbeom Cha <uiurihappy@sju.ac.kr>
2022-02-22 16:51:36 +0900

    feat: ajax 처리

--- app.js
+++ app.js
@@ -27,3 +27,10 @@ app.post("/email_post", function (req, res) {
   //res.send("<h1>welcome! " + req.body.email + "</h1>");
   res.render("email.ejs", { email: req.body.email });
 });
+
+app.post("/ajax_send_email", function (req, res) {
+  console.log(req.body.email);
+  //check validation about input value -> select db
+  var responseData = { result: "ok", email: req.body.email };
+  res.json(responseData);
+});
