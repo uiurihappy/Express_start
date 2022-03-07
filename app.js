cb6d74fd2fc1a1e701e7801b777f99aad7a5cbde
Yunbeom Cha <uiurihappy@sju.ac.kr>
2022-03-07 15:18:02 +0900

    feat: mysql 연동

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ var express = require("express");
 const res = require("express/lib/response");
 var app = express();
 var bodyParser = require("body-parser");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "1234",
+  database: "ybcha",
+});
+connection.connect();
 
 app.listen(3000, function () {
   console.log("start!!! express server on port 3000");
@@ -30,7 +40,21 @@ app.post("/email_post", function (req, res) {
 
 app.post("/ajax_send_email", function (req, res) {
   console.log(req.body.email);
-  //check validation about input value -> select db
-  var responseData = { result: "ok", email: req.body.email };
-  res.json(responseData);
+  var email = req.body.email;
+  var responseData = {};
+
+  var query = connection.query(
+    'select name from user where email="' + email + '"',
+    function (err, rows) {
+      if (err) throw err;
+      if (rows[0]) {
+        responseData.result = "ok";
+        responseData.name = rows[0].name;
+      } else {
+        responseData.result = "none";
+        responseData.name = "";
+      }
+      res.json(responseData);
+    }
+  );
 });
